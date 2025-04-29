742bcd05a75443ac2c2748e1daa6176845355343
Angel castañeda <luis.castaneda@noktos.com>
2025-04-29 13:14:56 -0600

    DONE

--- app/dashboard/cuentas-cobrar/page.tsx
+++ app/dashboard/cuentas-cobrar/page.tsx
@@ -3,8 +3,7 @@ import { TravelersPage } from "./_components/traveler_main";
 import { API_KEY } from "../../constants/constantes";
 
 export default async function TravelerDashboard() {
-
-  const endpoint = "http://localhost:3001/v1/mia/pagos/allPendientes";
+  const endpoint = "https://mianoktos.vercel.app/v1/mia/pagos/allPendientes";
   try {
     const response = await fetch(endpoint, {
       method: "GET",
